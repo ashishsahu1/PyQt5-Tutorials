f296b77a72a4bcdea743e4379aab1b947f58fdab
Ashish Sahu <ashishsahu1999@hotmail.com>
2021-02-13 20:09:35 +0530

    some more functions of QWidgets

--- Introduction/FirstWindowUsingOOP.py
+++ Introduction/FirstWindowUsingOOP.py
@@ -10,6 +10,11 @@ class WindowExample(QWidget):
         self.setGeometry(200,200,400,300)
         self.setWindowTitle("First Title")
         self.setWindowIcon(QIcon("../Images/design.png"))
+
+        # self.setFixedHeight(400)
+        # self.setFixedWidth(300)
+        self.setWindowOpacity(0.95)
+        self.setStyleSheet('background-color:#2A1E70')
         self.show()
 
 #creating our application
