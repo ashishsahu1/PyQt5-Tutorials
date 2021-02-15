5fdc5ade5dfc9526afa8c50263f8f6ec0d77b97b
Ashish Sahu <ashishsahu1999@hotmail.com>
2021-02-15 23:51:21 +0530

    press button slot added

--- MiniProject1/Calculator.py
+++ MiniProject1/Calculator.py
@@ -9,6 +9,8 @@ class MainWindow(qtw.QWidget):
         self.setLayout(qtw.QVBoxLayout())
 
         self.keypad()
+        self.tempNums = []
+        self.finNums = []
 
         self.show()
 
@@ -16,26 +18,26 @@ class MainWindow(qtw.QWidget):
         container= qtw.QWidget()
         container.setLayout(qtw.QGridLayout())
 
-        resultField = qtw.QLineEdit()
-        btnResult = qtw.QPushButton('Enter')
-        btnClear = qtw.QPushButton('Clear')
-        btn9 = qtw.QPushButton('9')
-        btn8 = qtw.QPushButton('8')
-        btn7 = qtw.QPushButton('7')
-        btn6 = qtw.QPushButton('6')
-        btn5 = qtw.QPushButton('5')
-        btn4 = qtw.QPushButton('4')
-        btn3 = qtw.QPushButton('3')
-        btn2 = qtw.QPushButton('2')
-        btn1 = qtw.QPushButton('1')
-        btn0 = qtw.QPushButton('0')
-        btnAdd = qtw.QPushButton('+')
-        btnSub = qtw.QPushButton('-')
-        btnMul = qtw.QPushButton('*')
-        btnDiv = qtw.QPushButton('/')
+        self.resultField = qtw.QLineEdit()
+        btnResult = qtw.QPushButton('Enter',clicked = lambda:self.resultField.setText('Test') )
+        btnClear = qtw.QPushButton('Clear', clicked = lambda:self.resultField.setText(''))
+        btn9 = qtw.QPushButton('9', clicked = lambda:self.numPress('9'))
+        btn8 = qtw.QPushButton('8', clicked = lambda:self.numPress('8'))
+        btn7 = qtw.QPushButton('7', clicked = lambda:self.numPress('7'))
+        btn6 = qtw.QPushButton('6', clicked = lambda:self.numPress('6'))
+        btn5 = qtw.QPushButton('5', clicked = lambda:self.numPress('5'))
+        btn4 = qtw.QPushButton('4', clicked = lambda:self.numPress('4'))
+        btn3 = qtw.QPushButton('3', clicked = lambda:self.numPress('3'))
+        btn2 = qtw.QPushButton('2', clicked = lambda:self.numPress('2'))
+        btn1 = qtw.QPushButton('1', clicked = lambda:self.numPress('1'))
+        btn0 = qtw.QPushButton('0', clicked = lambda:self.numPress('0'))
+        btnAdd = qtw.QPushButton('+', clicked = lambda:self.funPress('+'))
+        btnSub = qtw.QPushButton('-', clicked = lambda:self.funPress('-'))
+        btnMul = qtw.QPushButton('*', clicked = lambda:self.funPress('*'))
+        btnDiv = qtw.QPushButton('/',clicked = lambda:self.funPress('/'))
 
         #adding the buttons to the layout
-        container.layout().addWidget(resultField,0,0,1,4)
+        container.layout().addWidget(self.resultField,0,0,1,4)
         container.layout().addWidget(btnResult,1,0,1,2)
         container.layout().addWidget(btnClear,1,2,1,2)
         container.layout().addWidget(btn9,2,0)
@@ -53,11 +55,23 @@ class MainWindow(qtw.QWidget):
         container.layout().addWidget(btn0,5,0,1,3)
         container.layout().addWidget(btnDiv,5,3)
 
-
         self.layout().addWidget(container)
+    
+    def numPress(self,key):
+        self.tempNums.append((key))
+        tempStr = ''.join(self.tempNums)
+        if self.finNums:
+            self.resultField.setText(''.join(self.finNums)+tempStr)
+        else:
+            self.resultField.setText(tempStr)
 
+    def funPress(self,operator):
+        tempStr = ''.join(self.tempNums)
+        self.finNums.append(tempStr)
+        
 
 app = qtw.QApplication([])
 mw =  MainWindow()
+app.setStyle(qtw.QStyleFactory.create('Fusion'))
 
 app.exec_()
